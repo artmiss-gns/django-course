b2ecd5f29ea3c76afb5ec91847ac260e71285f2e
Artmiss G <artmiss1380@gmail.com>
2024-06-15 13:23:55 +0330

    project/personal-blog : build database models and admin models

--- project-building-blog/personal_blog/blog/admin.py
+++ project-building-blog/personal_blog/blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Author, Tag, Post
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email_add')
+    search_fields = ('first_name', 'last_name', 'email_add')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'excerpt', 'date', 'author', 'tag_list')
+    search_fields = ('title', 'excerpt', 'content')
+    filter_horizontal = ('tag',)
+    prepopulated_fields = {"slug": ("title", )}
+
+    def tag_list(self, obj):
+        return ', '.join([tag.name for tag in obj.tag.all()])
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
--- project-building-blog/personal_blog/blog/models.py
+++ project-building-blog/personal_blog/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from personal_blog.settings import BASE_DIR
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_add = models.EmailField()
+    
+    def __str__(self) -> str:
+        return f"{self.first_name}  {self.last_name}"
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=16)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=32)
+    excerpt = models.CharField(max_length=64)
+    image_add = models.FilePathField(path=BASE_DIR/'blog'/'static'/'blog'/'images',null=True)
+    date = models.DateField(null=False)
+    slug = models.SlugField()
+    content = models.TextField(null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, null=True)
+    
+    
+    def __str__(self) -> str:
+        return f"Post: {self.title}"
