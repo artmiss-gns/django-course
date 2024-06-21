87d1cdb0bf8722a11e7610f883f89c515f9c97cf
Artmiss G <artmiss1380@gmail.com>
2024-06-22 02:54:37 +0330

    Forms/added css files

--- Forms/feedback/reviews/forms.py
+++ Forms/feedback/reviews/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ReviewForm(forms.Form):
     name = forms.CharField(max_length=24)
     last_name = forms.CharField(max_length=32)
-    rating = forms.IntegerField(min_value=1, max_value=5, initial=3)
+    rating = forms.IntegerField(min_value=1, max_value=5, required=True)
     review = forms.CharField(widget=forms.Textarea, required=False)  
