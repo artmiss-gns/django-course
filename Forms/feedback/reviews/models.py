8558a5e3c48be3d1ea6fb15209a86ed9dc634705
Artmiss G <artmiss1380@gmail.com>
2024-06-23 03:10:07 +0330

    Forms/used FormModels for Form validation

--- Forms/feedback/reviews/models.py
+++ Forms/feedback/reviews/models.py
@@ -5,4 +5,7 @@ class Review(models.Model):
     name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     rating = models.IntegerField()
-    review = models.TextField()
+    review = models.TextField(null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return f"{self.pk}:{self.name}-{self.last_name}"
