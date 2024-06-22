f2b00cbda53d375e2a5dc390f202cfbb58b70376
Artmiss G <artmiss1380@gmail.com>
2024-06-22 04:03:28 +0330

    Forms/connected the data to the database

--- Forms/feedback/reviews/models.py
+++ Forms/feedback/reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    rating = models.IntegerField()
+    review = models.TextField()
--- Forms/feedback/reviews/views.py
+++ Forms/feedback/reviews/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from .forms import ReviewForm
-
+from .models import Review
 
 def review(request):
     if request.method == "GET":
@@ -12,6 +12,8 @@ def review(request):
     elif request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
+            review = Review(**form.cleaned_data)
+            review.save()
             return HttpResponseRedirect('thank_you')
         
 def thank_you(request):
