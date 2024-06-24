d8b08e74cc73d69ba620935b35aa0b08360f8225
Artmiss G <artmiss1380@gmail.com>
2024-06-25 00:55:32 +0330

    ClassViews/ TemplateView

--- ClassViews/feedback/reviews/urls.py
+++ ClassViews/feedback/reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import review, ThankYou
+from .views import review, ThankYou, ListReviews
 
 urlpatterns = [
     path('', review.as_view()),
-    path('thank_you', ThankYou.as_view(), name='thank_you_page')
+    path('thank_you', ThankYou.as_view(), name='thank_you_page'),
+    path('list_reviews', ListReviews.as_view()),
 ]
--- ClassViews/feedback/reviews/views.py
+++ ClassViews/feedback/reviews/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
@@ -23,3 +24,13 @@ class review(View):
 
 class ThankYou(TemplateView):
     template_name='reviews/thank_you.html'
+    
+    
+class ListReviews(TemplateView):
+    template_name='reviews/review_list.html'
+    
+    def get_context_data(self, **kwargs):
+        reviews = Review.objects.all()
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = reviews
+        return context
