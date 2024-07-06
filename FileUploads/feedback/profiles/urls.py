13f114aa092f6f706ecc4585bb09d953d9aac778
Artmiss G <artmiss1380@gmail.com>
2024-07-06 20:14:57 +0330

    FileUploads/ used TemplateView

--- FileUploads/feedback/profiles/urls.py
+++ FileUploads/feedback/profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CreateProfileView.as_view())
+    path("", views.CreateProfileView.as_view()),
+    path('success_upload', views.SuccessView.as_view()),
 ]
--- FileUploads/feedback/profiles/views.py
+++ FileUploads/feedback/profiles/views.py
@@ -1,33 +1,47 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+
 from .forms import FileUploadForm
 from .models import UserImage
 
-def save_file(file):
-    file_postfix = file.__str__().partition('.')[-1]
-    with open(f'tmp/image.{file_postfix}', 'wb+') as new_file :
-        for chunk in file.chunks() :
-            new_file.write(chunk)
+# def save_file(file):
+#     file_postfix = file.__str__().partition('.')[-1]
+#     with open(f'tmp/image.{file_postfix}', 'wb+') as new_file :
+#         for chunk in file.chunks() :
+#             new_file.write(chunk)
             
-class CreateProfileView(View):
-    def get(self, request):
-        form = FileUploadForm()
-        return render(
-            request, "profiles/create_profile.html",
-            context={'form': form}
-        )
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = FileUploadForm()
+#         return render(
+#             request, "profiles/create_profile.html",
+#             context={'form': form}
+#         )
     
-    def post(self, request):
-        submitted_form = FileUploadForm(request.POST, request.FILES)
+#     def post(self, request):
+#         submitted_form = FileUploadForm(request.POST, request.FILES)
 
-        if submitted_form.is_valid():
-            image = UserImage(image=submitted_form.cleaned_data['user_image'])
-            image.save()
-            return render(request, 'profiles/success_upload.html')
-        else :
-            return render(
-                request, "profiles/create_profile.html",
-                context={'form': submitted_form}
-            )
+#         if submitted_form.is_valid():
+#             image = UserImage(image=submitted_form.cleaned_data['user_image'])
+#             image.save()
+#             return render(request, 'profiles/success_upload.html')
+#         else :
+#             return render(
+#                 request, "profiles/create_profile.html",
+#                 context={'form': submitted_form}
+#             )
+
+
+class CreateProfileView(CreateView):
+    model = UserImage
+    template_name = "profiles/create_profile.html"
+    success_url = "success_upload"
+    fields = "__all__"
+    
+    
+class SuccessView(TemplateView):
+    template_name = 'profiles/success_upload.html'
