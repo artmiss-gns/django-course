7e5669cdd9cc04e9ff46320bd8ccde7bbebefc96
Artmiss G <artmiss1380@gmail.com>
2024-07-06 18:47:35 +0330

    FileUploads/ manual saving the uploaded file

--- FileUploads/feedback/profiles/views.py
+++ FileUploads/feedback/profiles/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-
+def save_file(file):
+    file_postfix = file.__str__().partition('.')[-1]
+    with open(f'tmp/image.{file_postfix}', 'wb+') as new_file :
+        for chunk in file.chunks() :
+            new_file.write(chunk)
+            
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        print(request.POST)
-        return HttpResponseRedirect('/profiles')
+        save_file(request.FILES["profile_picture"])
+        return render(request, 'profiles/success_upload.html')
